37f61579d2d3eaca574e37432f1042bb2b824569
Tiffany A. Smith <97918050+tiffanyautumn@users.noreply.github.com>
2022-07-22 16:24:46 -0500

    Revert "day one modules and task list"

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import { createRoot } from "react-dom/client"
-import "./index.css"
-import { BrowserRouter } from "react-router-dom"
-import { Doves } from "./components/Doves.js"
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById("root")
-const root = createRoot(container)
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <Doves />
-    </BrowserRouter>
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
